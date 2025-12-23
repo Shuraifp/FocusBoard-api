6868b7391d15512d083632c030731da6112d4e67
“Shuraifp” <msharraf258@gmail.com>
2025-12-23 09:02:29 +0530

    test: setup scaffold, add setup.ts

---
+++ tests/setup.ts
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+import { MongoMemoryServer } from "mongodb-memory-server";
+
+let mongoServer: MongoMemoryServer;
+
+beforeAll(async () => {
+  mongoServer = await MongoMemoryServer.create();
+  const mongoUri = mongoServer.getUri();
+
+  await mongoose.connect(mongoUri);
+  console.log('✅ Connected to in-memory test database');
+});
+
+beforeEach(async () => {
+  const collections = mongoose.connection.collections;
+
+  for (const key in collections) {
+    const collection = collections[key];
+    await collection.deleteMany({});
+  }
+});
+
+afterAll(async () => {
+  await mongoose.connection.dropDatabase();
+  await mongoose.connection.close();
+  await mongoServer.stop();
+  console.log('✅ Disconnected from test database');
+});
